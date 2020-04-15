f7c76b9a3a442ceba1533f806ee203a802f9179a
Tyler McWilliams <tyler.ellis.mcwilliams@gmail.com>
2020-04-15 11:12:14 +0200

    holding list till savegame

--- LookingForGroup/src/LookingForGroup.cs
+++ LookingForGroup/src/LookingForGroup.cs
@@ -8,80 +8,78 @@ namespace Vintagestory.ServerMods
     class LookingForGroup : ModSystem
     {
         ICoreServerAPI serverApi;
+        List<string> lfgList;
         public override void StartServerSide(ICoreServerAPI api)
         {
             serverApi = api;
 
-            api.RegisterCommand("lfg", "List or join the lfg list", "[list|join|leave]", OnLfg);
+            api.Event.GameWorldSave += OnSaveGameSaving;
+            api.Event.SaveGameLoaded += OnSaveGameLoading;
+
+            api.RegisterCommand("lfg", "Access the lfg list.", "[list|join|leave]", OnLfg);
         }
 
         private void OnLfg(IServerPlayer player, int groupId, CmdArgs args)
         {
-            if (args.Length < 1)
-            {
-                player.SendMessage(groupId, "/lfg [list|join|leave]", EnumChatType.CommandError);
-                return;
-            }
-
-            byte[] data = serverApi.WorldManager.SaveGame.GetData("lfg");
-
-            List<string> players = data == null ? new List<string>() : SerializerUtil.Deserialize<List<string>>(data);
-
             string cmd = args.PopWord();
-
             switch (cmd)
             {
                 case "list":
-                    if (players.Count == 0)
+                    if (lfgList.Count == 0)
                     {
-                        player.SendMessage(groupId, "Noone is looking for group!", EnumChatType.Notification);
-                        break;
-                    }
-
-                    string lfgList = "Players looking for group:";
-                    players.ForEach((playerUid) =>
-                    {
-                        lfgList += "\n" + serverApi.World.PlayerByUid(playerUid).PlayerName;
-                    });
-
-                    player.SendMessage(groupId, lfgList, EnumChatType.Notification);
-                    break;
-
-                case "join":
-                    if (players.Contains(player.PlayerUID))
-                    {
-                        player.SendMessage(groupId, "You're already in this list!", EnumChatType.Notification);
+                        player.SendMessage(groupId, "Noone is looking for a group.", EnumChatType.CommandSuccess);
                     }
                     else
                     {
-                        players.Add(player.PlayerUID);
-                        data = SerializerUtil.Serialize(players);
+                        string response = "Players looking for group:";
+                        lfgList.ForEach((playerUid) =>
+                        {
+                            response += "\n" + serverApi.World.PlayerByUid(playerUid).PlayerName;
+                        });
 
-                        serverApi.WorldManager.SaveGame.StoreData("lfg", data);
+                        player.SendMessage(groupId, response, EnumChatType.CommandSuccess);
+                    }
+                    break;
 
-                        player.SendMessage(groupId, "Successfully joined!", EnumChatType.Notification);
+                case "join":
+                    if (lfgList.Contains(player.PlayerUID))
+                    {
+                        player.SendMessage(groupId, "You're already in the list!", EnumChatType.CommandError);
+                    }
+                    else
+                    {
+                        lfgList.Add(player.PlayerUID);
+                        player.SendMessage(groupId, "Successfully joined.", EnumChatType.CommandSuccess);
                     }
                     break;
 
                 case "leave":
-                    if (!players.Remove(player.PlayerUID))
+                    if (!lfgList.Remove(player.PlayerUID))
                     {
-                        player.SendMessage(groupId, "You're not in the list!", EnumChatType.Notification);
+                        player.SendMessage(groupId, "You're not in the list!", EnumChatType.CommandError);
                     }
                     else
                     {
-                        data = SerializerUtil.Serialize(players);
-
-                        serverApi.WorldManager.SaveGame.StoreData("lfg", data);
-
-                        player.SendMessage(groupId, "Successfully left!", EnumChatType.Notification);
+                        player.SendMessage(groupId, "Successfully left.", EnumChatType.CommandSuccess);
                     }
                     break;
 
+                case null:
                 default:
                     player.SendMessage(groupId, "/lfg [list|join|leave]", EnumChatType.CommandError);
-                    break;
+                    return;
             }
+        }
+
+        private void OnSaveGameLoading()
+        {
+            byte[] data = serverApi.WorldManager.SaveGame.GetData("lfg");
+
+            lfgList = data == null ? new List<string>() : SerializerUtil.Deserialize<List<string>>(data);
+        }
+        private void OnSaveGameSaving()
+        {
+            serverApi.WorldManager.SaveGame.StoreData("lfg", SerializerUtil.Serialize(lfgList));
         }
     }
 }
